53718638e644109c6c4ecb3f16583416f3262eac
96isasva <mustigmat@gmail.com>
2015-09-23 16:01:08 +0200

    lektion 23/9

--- crop.py
+++ crop.py
@@ -5,11 +5,12 @@ Created on Wed Sep 16 14:59:59 2015
 @author: 96isasva
 """
 import math
+import random
 class Crop:
     def __init__(self,growth_rate, light_need, water_need):
         
         self._growth = 0
-        self._days_groowing = 0
+        self._days_growing = 0
         self._growth_rate = growth_rate
         self._light_need = light_need
         self._water_need = water_need
@@ -25,19 +26,29 @@ class Crop:
         self._growth = value
     def get_growth(self):
         return (self._growth)
+    def _update_status(self):
+        if self._growth > 15:
+            self._status = "old"
+        
+        
+    def grow(self,light,water):
+        if light >= self._light_need and water >= self._water_need:
+            self._growth += self._growth_rate
+        self._days_growing += 1
+        self._update_status()
+    
+def autogrow(crop,days):
+    for day in range(days):
+        light = random.randint(1,10)
+        water = random.randint(1,10)
+        crop.grow(light,water)
 
 def main():
-    new_crop = Crop(1,4,3)
-    print(new_crop.needs())
+    new_crop = Crop(1,5,9)
+    autogrow(new_crop,30)
+    print(new_crop._status)
     print(new_crop.report())
-    new_crop2 = Crop(1,5,9)
-    print(new_crop2._status)
-    print(new_crop2._light_need)
-    print(new_crop2._water_need)
-    help(Crop)
-    help(math)
-    print(math)
-    new_crop.set_growth(5)
+
     
 if __name__ == "__main__":
     main()
