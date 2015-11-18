030c5e2b4bed2588ad259b53be45a34d1e4bed61
96isasva <mustigmat@gmail.com>
2015-11-18 16:06:39 +0100

    lektion 18/11

--- exam1/__init__.py
+++ exam1/__init__.py
@@ -20,6 +20,43 @@ class Exam(QMainWindow):
     def initUI(self):
         self.frame = QWidget(self)
         self.setCentralWidget(self.frame)
+        self.layout = QVBoxLayout()
+        self.form_layout = QFormLayout()
+        
+        self.inputs = QTextEdit(self)
+        self.inputs.setReadOnly(True)
+        self.form_layout.addRow('Inputs: ',self.inputs)
+        
+        self.name = QLineEdit(self)
+        self.name.setMinimumWidth(130)
+        self.form_layout.addRow('Input: ', self.name)
+        self.name.move(50,50)
+        #self.led.returnPressed.connect()
+        
+        self.inputs = QTextEdit(self)
+        self.inputs.setReadOnly(True)
+        self.form_layout.addRow('Inputs: ',self.inputs)
+        
+        self.name = QLineEdit(self)
+        self.name.setMinimumWidth(130)
+        self.form_layout.addRow('Input: ', self.name)
+        self.name.move(300,50)
+        #self.led.returnPressed.connect()
+        
+        
+        self.btn_ok = QPushButton("ok",self)
+        self.btn_ok.move(150,150)
+        #self.btn_ok.clicked.connect()
+        
+        self.btn_clear = QPushButton("rensa",self)
+        self.btn_clear.move(100,100)
+        #self.btn_ok.clicked.connect()
+        
+
+        
+        
+        
+        
         
     def run(self):
         self.show()
